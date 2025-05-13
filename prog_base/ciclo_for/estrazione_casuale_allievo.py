408199be5b32593f1074f8b58e79c42e6f3bd262
Tyler <davidedifilippo@iisdaostalaquila.it>
2025-05-13 17:05:42 +0200

    Update estrazione_casuale_allievo.py

--- prog_base/ciclo_for/estrazione_casuale_allievo.py
+++ prog_base/ciclo_for/estrazione_casuale_allievo.py
@@ -1,13 +1,15 @@
 import random
 
+#le liste sono sequenze di elementi tra parentesi quadre separati da virgole
 lista_allievi = ["Alice", "Marco", "Carlo", "David", "Eva", "Franco", "Grazia", "Enrico", "Asia", "Mirko"]
 
+#per ogni allievo nella lista stampo il nome
 for allievo in lista_allievi:
     print(allievo)
 
 allievo_estratto = random.choice(lista_allievi)
 
-print("L'allievo estratto casualmente è:" allievo_estratto)
+print("L'allievo estratto casualmente è:", allievo_estratto)
 
 lista_allievi.remove(allievo_estratto)
 
